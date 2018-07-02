54471c4694452ef2f755bb8f116b0fbcb288c502
general <general@MacBook-Pro-2.local>
2018-07-02 13:48:58 -0700

    added main routine and started psuedo

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fbs"
+	"fmt"
+	"github.com/kataras/iris"
+)
+
+func main() {
+	fbs.FBS()
+	fmt.Println("10")
+}
+
+//Todo: Get user input and compute projections to return a data visualization.
+
+type State struct {
+
+}
+
+type Entity struct {
+	id string
+	fullName string
+	history map[string] State
+	state State
+}
+
+type TaxCodeConfigSpec struct {
+	currentProcess int
+}
+
+type TaxCodeConfigs struct {
+	entity Entity
+	inProgress bool
+	pristine bool
+	old bool
+	specs TaxCodeConfigSpec
+}
+
+
+type Store struct {
+	value func(key string)
+	next func()
+	dispatch func()
+	_source map[string] string
+}
+
+func StoresFactory(configs TaxCodeConfigs) {
+	// Todo return a stores obj
+}
+
+// TODO: Main UX Flow ( Init UI -> Consumes Profile Data -> Sets Session State -> Retrieves Process Library -> Assembles Processes -> Project )
+
+func initiallizeUI() {
+	const app = iris.Default()
+}
+
+// TODO: Homeostasis do while ( Resilience is > critical , no Sig Term)
+
+// TODO: Homeostatic functions ( Health check -> audit servers -> forEach() { compare with manifest IF not normal send report/alert } )
+
+// TODO: Internal functions ( Init Data Update -> get list of document URLs -> { init crawler -> retrieve docs and format -> parse -> store processes } -> log histories -> ... )
+
+// TODO: Internal checks -> IF not normal send report/alert
+
+// TODO: Alerts/Reports ->
+
+// TODO: API ( Processes Library, Crawler, Reflection, Session, Store )
